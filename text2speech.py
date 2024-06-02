1e9757f985d24422f92106b0aaf72563ec69969c
Vaghela divyesh <86907707+iamdivyeshh@users.noreply.github.com>
2024-06-02 20:04:36 +0530

    Text To Speech

---
+++ text2speech.py
@@ -0,0 +1,12 @@
+import pyttsx3
+
+def speech2txt(x):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice',voices[1].id)
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate',150)
+    engine.say(x)
+    engine.runAndWait()
+
+speech2txt("hello welcome back divyeshh")
